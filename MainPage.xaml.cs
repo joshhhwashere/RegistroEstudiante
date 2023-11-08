93254462859f70a54931ab5afbb0d10460766fd6
joshwashere <joshua.gonzalez@tecnologicoliceocristiano.edu.ec>
2023-11-07 20:53:12 -0500

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿namespace RegistroEstudiante
+{
+    public partial class MainPage : ContentPage
+    {
+        private string titulo = "Registro";
+        public Estudiante MiEstudiante { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            MiEstudiante = new Estudiante();
+            BindingContext = MiEstudiante;
+        }
+
+        private void ConfirmarBtn_Clicked(object sender, EventArgs e)
+        {
+            if (IsValid())
+            {
+                var mensaje = $"{MiEstudiante.Nombres} sus datos se ingresaron correctamente.";
+
+                DisplayAlert(titulo, mensaje, "Ok");
+            }
+            
+        }
+        private bool IsValid()
+        {
+            if (string.IsNullOrEmpty(MiEstudiante.Nombres))
+            {
+                DisplayAlert(titulo, "Debe Ingresar un Nombre", "Ok");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(MiEstudiante.Apellidos))
+            {
+                DisplayAlert(titulo, "Debe Ingresar un Apellido", "Ok");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(MiEstudiante.Materia))
+            {
+                DisplayAlert(titulo, "Debe Ingresar un Materia", "Ok");
+                return false;
+            }
+
+            return true; 
+        }
+    }
+}
